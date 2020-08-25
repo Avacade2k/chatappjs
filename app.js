84e81acd1b9cac0cd3319cdd2f3c5c462236a7ee
Avacade2k <57358089+Avacade2k@users.noreply.github.com>
2020-08-25 14:20:02 +0200

    d2

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ const peerOnOpen = (id) => {
 const myPeerId = location.hash.slice(1);
 console.log(myPeerId);
 
+const peerOnError = (error) => {
+    console.log(error);
+};
+
 let peer = new Peer(myPeerId, {
     host:"glajan.com",
     port: 8443,
@@ -12,3 +16,29 @@ let peer = new Peer(myPeerId, {
     secure: true,
 });
 peer.on("open", peerOnOpen);
+peer.on("error", peerOnError);
+
+document.querySelector('.list-all-peers-button').addEventListener('click', () => {
+    const peersEl = document.querySelector('.peers');
+    const ul = document.createElement('ul');
+    peer.listAllPeers((peers) => {
+        peers.filter((peerId) => peerId !== myPeerId)
+        .forEach(peerId => {
+            const li = document.createElement('li');
+            const button = document.createElement('button');
+            button.innerText = peerId;
+            button.classList.add("connect-button");
+            button.classList.add(`peerId-´$(peerId)`);
+            li.appendChild(button);
+            ul.appendChild(li);
+        });
+        console.log(peers);
+        peersEl.appendChild(ul);
+    });
+});
+
+let bruh = (a, b) => a + b;
+
+function isEven(a) {
+    return a % 2 == 0;
+}
