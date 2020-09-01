e10252c2d3e7914e01b5d3fbc349d6f501dfc457
Avacade2k <57358089+Avacade2k@users.noreply.github.com>
2020-09-01 13:16:16 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,38 +1,95 @@
-const peerOnOpen = (id) => {
-    document.querySelector('.my-peer-id').innerHTML = id;
-};
+(function () {
+  let peer = null;
+  let conn = null;
+  const peerOnOpen = (id) => {
+    document.querySelector(".my-peer-id").innerHTML = id;
+  };
 
-const myPeerId = location.hash.slice(1);
-console.log(myPeerId);
+  const peerOnError = (err) => {
+    console.log(err);
+  };
 
-const peerOnError = (error) => {
-    console.log(error);
-};
+  const myPeerId = location.hash.slice(1);
+  console.log(myPeerId);
 
-let peer = new Peer(myPeerId, {
-    host:"glajan.com",
+  const peerOnConnection = (dataConnection) => {
+    conn && conn.close();
+    conn = dataConnection;
+    console.log(dataConnection);
+
+    const event = new CustomEvent("peer-changed", {
+      detail: { peerId: dataConnection.peer },
+    });
+    document.dispatchEvent(event);
+    conn.on("data", (data) => {
+      console.log(data);
+    });
+  };
+
+  const connectToPeerClick = (el) => {
+    const peerId = el.target.textContent;
+    conn && conn.close();
+    conn = peer.connect(peerId);
+    conn.on("open", () => {
+      console.log("connected");
+      const event = new CustomEvent("peer-changed", {
+        detail: { peerId: peerId },
+      });
+      document.dispatchEvent(event);
+    });
+  };
+
+  peer = new Peer(myPeerId, {
+    host: "glajan.com",
     port: 8443,
-    path:"/myapp",
+    path: "/myapp",
     secure: true,
-});
-peer.on("open", peerOnOpen);
-peer.on("error", peerOnError);
+    config: {
+      iceServers: [
+        { url: ["stun:eu-turn7.xirsys.com"] },
+        {
+          username:
+            "1FOoA8xKVaXLjpEXov-qcWt37kFZol89r0FA_7Uu_bX89psvi8IjK3tmEPAHf8EeAAAAAF9NXWZnbGFqYW4=",
+          credential: "83d7389e-ebc8-11ea-a8ee-0242ac140004",
+          url: "turn:eu-turn7.xirsys.com:80?transport=udp",
+        },
+      ],
+    },
+  });
 
-document.querySelector('.list-all-peers-button').addEventListener('click', () => {
-    const peersEl = document.querySelector('.peers');
-    const ul = document.createElement('ul');
-    peer.listAllPeers((peers) => {
-        peers.filter((peerId) => peerId !== myPeerId)
-        .forEach(peerId => {
-            const li = document.createElement('li');
-            const button = document.createElement('button');
+  peer.on("open", peerOnOpen);
+  peer.on("error", peerOnError);
+  peer.on("connection", peerOnConnection);
+
+  document
+    .querySelector(".list-all-peers-button")
+    .addEventListener("click", () => {
+      const peersEl = document.querySelector(".peers");
+      peersEl.firstChild && peersEl.firstChild.remove();
+      peer.listAllPeers((peers) => {
+        const ul = document.createElement("ul");
+        peers
+          .filter((peerId) => peerId !== myPeerId)
+          .forEach((peerId) => {
+            const li = document.createElement("li");
+            const button = document.createElement("button");
             button.innerText = peerId;
             button.classList.add("connect-button");
-            button.classList.add(`peerId-$(peerId)`);
+            button.classList.add(`peerId-${peerId}`);
+            button.addEventListener("click", connectToPeerClick);
             li.appendChild(button);
             ul.appendChild(li);
-        });
-        console.log(peers);
+          });
         peersEl.appendChild(ul);
+      });
     });
-});
+  document.addEventListener("peer-changed", (e) => {
+    const peerId = e.detail.peerId;
+    console.log(peerId);
+    let peerIdClass = ".peerId-" + peerId;
+    document.querySelectorAll(".connect-button").forEach((el) => {
+      el.classList.remove("connected");
+    });
+    document.querySelector(peerIdClass).classList.add("connected");
+  });
+})();
